655a7bcaa9a12b2cd2e81c33f495f533ad6c070d
biongodev <biongo.dev@gmail.com>
2024-09-16 13:11:56 +0900

    Set up GitHub workflows to automatically deploy the project.

--- scripts/env.js
+++ scripts/env.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-// 从环境变量中获取 SSH_KNOWN_HOST
-const test = process.env.TEST;
-
-// 定义 .env 文件的路径
+// 定义 .env 文件的路径（主目录）
 const envFilePath = path.join(__dirname, '..', '.env');
 
 // 准备要写入 .env 文件的内容
-const envContent = `TEST=${test}\n`;
+const envContent = Object.entries(process.env)
+  .map(([key, value]) => `${key}=${value}`)
+  .join('\n');
+
+console.log({ envContent });
 
 // 将内容写入 .env 文件
 fs.writeFile(envFilePath, envContent, (err) => {
   if (err) {
     console.error('Error writing to .env file', err);
   } else {
-    console.log('.env file created successfully!', String(test).length);
+    console.log('.env file created successfully!');
   }
 });
