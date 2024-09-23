849050680a29d74a5c1b240876a3036177c76139
biongodev <biongo.dev@gmail.com>
2024-09-23 17:09:24 +0900

    Add Music section into page profile.

--- src/components/music-section.tsx
+++ src/components/music-section.tsx
@@ -3,14 +3,14 @@ import WebsiteContainer from './website/website-container';
 
 export default function MusicSection() {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden h-[1000px]">
       <div
         style={{
           width: '75%',
           height: 'auto',
           position: 'relative',
           left: -300,
-          top: 300,
+          top: -10,
         }}
       >
         <Image
@@ -25,64 +25,57 @@ export default function MusicSection() {
       <div className="absolute left-0 top-0 w-full h-full">
         <WebsiteContainer>
           <div className="flex w-full gap-8">
-            <div className="basis-1/5 flex flex-col gap-6">
-              <div className="border  rounded-xl bg-[#283343] p-10 text-white flex flex-col gap-6">
-                <p>{'My favorite singer.'}</p>
-              </div>
-            </div>
-            <div className="basis-4/5 flex flex-col gap-6">
-              <div className="grid grid-cols-2 gap-4">
-                <div
-                  style={{
-                    width: 'auto',
-                    height: 'auto',
-                    position: 'relative',
-                  }}
-                >
-                  <Image
-                    className="rounded-xl"
-                    src="/higedan.jpg"
-                    layout="responsive"
-                    width={525}
-                    height={517}
-                    alt=""
-                  />
-                </div>
-
-                <div
-                  style={{
-                    width: 'auto',
-                    height: '340px',
-                    position: 'relative',
-                    overflow: 'hidden',
-                  }}
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+              <div
+                style={{
+                  width: 'auto',
+                  height: 'auto',
+                  position: 'relative',
+                }}
+              >
+                <Image
                   className="rounded-xl"
-                >
-                  <Image
-                    className="rounded-xl"
-                    src="/yoasobi.jpg"
-                    layout="responsive"
-                    width={525}
-                    height={517}
-                    alt=""
-                  />
-                </div>
-                <div
-                  style={{
-                    width: 'auto',
-                    height: 'auto',
-                    position: 'relative',
-                  }}
-                >
-                  <Image
-                    className="rounded-xl"
-                    src="/justin.jpg"
-                    layout="responsive"
-                    width={300}
-                    height={300}
-                    alt=""
-                  />
-                </div>
+                  src="/higedan.jpg"
+                  layout="responsive"
+                  width={525}
+                  height={517}
+                  alt=""
+                />
+              </div>
+
+              <div
+                style={{
+                  width: 'auto',
+                  height: '340px',
+                  position: 'relative',
+                  overflow: 'hidden',
+                }}
+                className="rounded-xl"
+              >
+                <Image
+                  className="rounded-xl"
+                  src="/yoasobi.jpg"
+                  layout="responsive"
+                  width={525}
+                  height={517}
+                  alt=""
+                />
+              </div>
+              <div
+                style={{
+                  width: 'auto',
+                  height: 'auto',
+                  position: 'relative',
+                }}
+              >
+                <Image
+                  className="rounded-xl"
+                  src="/justin.jpg"
+                  layout="responsive"
+                  width={300}
+                  height={300}
+                  alt=""
+                />
               </div>
             </div>
           </div>
